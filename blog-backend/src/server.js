62b47d8448c20150536bf164169688ec24274f17
balaji <86532365+HVBSL@users.noreply.github.com>
2024-01-19 13:23:46 +0000

    API errors

---
+++ blog-backend/src/server.js
@@ -0,0 +1,144 @@
+import Express from "express";
+import { db, connectToDB } from "./db.js";
+import axios from "axios";
+import cors from "cors"
+import bodyParser from "body-parser";
+import fs from "fs";
+import admin from "firebase-admin";
+
+const credentials = JSON.parse(
+    fs.readFileSync('./credentials.json')
+)
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+})
+
+const app = Express();
+
+// app.use(Express.json());
+// app.use(axios);
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(async(req,res,next)=>{
+    const {authToken }= req.headers;
+
+    if (authToken) {
+        try{
+            req.user= await admin.auth().verifyIdToken(authToken);
+        }
+        catch{
+            return res.sendStatus(400);
+        }
+    }
+    req.user = req.user||{}
+    next();
+})
+
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    console.log('Uncaught Exception... Shutting down');
+    process.exit(1);
+});
+
+
+app.get("/", (req, res) => {
+    console.log("Hello Guys");
+    res.send("Hello guys, thanks for your work")
+});
+
+app.get('/api/articles/:name/', async (req, res) => {
+    const { name } = req.params;
+    const {uid} = req.user;
+    console.log(req.user);
+
+    const article = await db.collection('articles').findOne({ name });
+    if(article) {
+        const upvoteIds = article.upvotes||[];
+        article.canUpvote = uid&&!upvoteIds.include(uid);
+        res.json(article).status(200) 
+        
+    }
+    else{
+        res.sendStatus(404);
+    }
+
+});
+
+app.use((req,res,next)=>{
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+})
+
+app.put('/api/articles/:name/upvotes', async (req, res, err) => {
+    const { name } = req.params;
+    const {uid} = req.user;
+    
+    //change later
+    const article = await db.collection('articles').findOne({ name });
+    
+    if (article) {
+        
+        const upvoteIds = article.upvotes||[];
+        const canUpvote = uid&&!upvoteIds.includes(uid);
+
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, { 
+                $inc: { upvotes: 1 }, 
+                $push: {upvoteIds: uid}})
+                .then(async () => {
+                    const updatedArticle = await db.collection('articles').findOne({ name });
+                    console.log(`The ${name} has ${article.upvotes} upvotes and ${article.downvotes} downvotes`);
+                    res.status(201).json(updatedArticle);
+                })
+                .catch(err => console.log(err));;
+        }
+    } else {
+        res.send('The Article does not exists');
+    }
+    
+
+
+});
+
+app.put('/api/articles/:name/downvotes', async (req, res) => {
+    const { name } = req.params;
+    await db.collection('articles').updateOne({ name }, { $inc: { downvotes: 1 } })
+        .then(async () => {
+            const article = await db.collection('articles').findOne({ name });
+            console.log(`The ${name} has ${article.upvotes} upvotes and ${article.downvotes} downvotes`);
+            res.status(201).json(article);
+
+        })
+        .catch(err => console.log(err));
+});
+
+app.post('/api/articles/:name/comments/', async (req, res) => {
+    const { name } = req.params;
+    const { comment } = req.body;
+    const { email } =req.user;
+    
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy: email, comment } },
+    });
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article) {
+        res.json(article); 
+    } else {
+        res.send('That article doesn\'t exist!');
+    }
+});
+
+connectToDB((err) => {
+    app.listen(8000, (err) => {
+        console.log("Server is running on the port 8000");
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log(err ? err : "Database is connected");
+});
--- blogfrontend/src/components/commentsList.js
+++ blogfrontend/src/components/commentsList.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-const CommentsList = ({ comments }) => {
-    if(comments){
-        return(
+const CommentsList = ({ comments }) => (
         <>
-        <h3 className="clow">Comments:</h3>
-        <br></br>
-        {comments.map((comment,key) => (
-            <div className="comment" key={key}>
-            <h4>{comment.postedBy}</h4>
-            <p>{comment.comment}</p>
-            </div>
-        ))}
-    </>
-  )}
-}
+            <h3 className="clow">Comments:</h3>
+            <br></br>
+            {comments&&comments.map((comment,key) => (
+                <div className="comment" key={key}>
+                <h4>{comment.postedBy}</h4>
+                <p>{comment.comment}</p>
+                </div>
+            ))}
+        </>
+    )
+  
+
 
 export default CommentsList;
